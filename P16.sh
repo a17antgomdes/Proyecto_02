e9fbe8b4f3a0081a07de432ab3e03ec41f9f00f2
Nilson Issaoui <a17nilissher@iam.cat>
2019-05-02 18:15:16 +0200

    Ya esta acabado

--- P16.sh
+++ P16.sh
@@ -8,8 +8,20 @@ if [ $(id -u) -eq 0 ]; then
 	password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '');
 	#echo $password;
 	egrep "^$username" /etc/passwd >/dev/null
-
-
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username 
+		#usermod -c $COMMENT -m $username
+		#chage -c $COMMENT $username
+		#useradd -c "${COMMENT}" -m ${username}
+		#usermod -c "${COMMENT}" -m ${username}
+		chfn -f $COMMENT $username
+		chage -d 0 $username
+		[ $? -eq 0 ] && echo "User has been added to system with his full name!" || echo "Failed to add a user!"
+	fi
 else
 	echo "Only root may add a user to the system"
 	exit 2
